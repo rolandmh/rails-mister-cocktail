1b36c3441b49b40653783ef178389b74029f7afd
Roland Rether <rolandmhcontact@gmail.com>
2019-05-23 14:16:11 +0800

    validation and associations added and tested

---
+++ app/models/dose.rb
@@ -0,0 +1,8 @@
+class Dose < ApplicationRecord
+  belongs_to :ingredient
+  belongs_to :cocktail
+
+  validates :description, presence: true
+  validates_uniqueness_of :cocktail_id, :scope => [:ingredient_id], presence: true
+  validates_uniqueness_of :ingredient_id, :scope => [:cocktail_id], presence: true
+end
