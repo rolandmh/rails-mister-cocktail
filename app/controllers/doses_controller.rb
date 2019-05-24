ea83787cf2125fe3ad1baeaef47c8cc18fa0a4b2
Roland Rether <rolandmhcontact@gmail.com>
2019-05-24 12:13:45 +0800

    added create dose and destroy dose

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,30 +1,34 @@
 class DosesController < ApplicationController
-  before_action :set_dose, only: [:destroy]
+  before_action :set_cocktail, only: [:new, :create]
 
   def new
     @dose = Dose.new
-    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
   def create
+    # @ingredient = Ingredient.find(params[:dose][:ingredient_id])
     @dose = Dose.new(dose_params)
-    @cocktail = Cocktail.find(params[:cocktail_id])
+
+    # @dose.ingredient = @ingredient
+    @dose.cocktail = @cocktail
+
     if @dose.save
-      redirect_to dose_path(@dose)
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
   end
 
   def destroy
-    @dose.delete
-    redirect_to dose_path
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
 
-  def set_dose
-    @dose = Dose.find(params[:id])
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
   def dose_params
